7b3517d42b8708adefe767e54aa28a2b615cf995
Bittu1712 <soumyadeepmajumder2001@gmail.com>
2026-03-23 16:59:01 +0530

    serve html via api routes

---
+++ api/admin-page.js
@@ -0,0 +1,9 @@
+import fs from 'fs';
+import path from 'path';
+
+export default function handler(req, res) {
+    const filePath = path.join(process.cwd(), 'public', 'admin.html');
+    const content = fs.readFileSync(filePath, 'utf8');
+    res.setHeader('Content-Type', 'text/html');
+    res.send(content);
+}
